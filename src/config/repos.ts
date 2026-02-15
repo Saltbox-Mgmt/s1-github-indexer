4587741b4a0383e4ed66a9f423015d7ca214ffe9
shane-saltbox <shane@saltboxmgmt.com>
2026-02-15 13:46:42 -0600

    Update to repos

--- src/config/repos.ts
+++ src/config/repos.ts
@@ -7,8 +7,21 @@ export interface RepoConfig {
 // Add your repos here
 export const REPO_ALLOWLIST: RepoConfig[] = [
   // Example:
+  { owner: 'Saltbox-Mgmt', repo: 'Saltbox-Base', branch: 'Master' },
+  { owner: 'Saltbox-Mgmt', repo: 'saltbox-ignition', branch: 'Master' },
+  { owner: 'Saltbox-Mgmt', repo: 'saltbox-throttle', branch: 'Master' },
+  { owner: 'Saltbox-Mgmt', repo: 'Saltbox-Advance', branch: 'Master' },
+  { owner: 'Saltbox-Mgmt', repo: 'Saltbox-SDO', branch: 'Master' },
   { owner: 'Saltbox-Mgmt', repo: 'AW-Parts-Store', branch: 'main' },
-  // { owner: 'Saltbox-Mgmt', repo: 'Saltbox-Base', branch: 'Dev' },
+  { owner: 'Saltbox-Mgmt', repo: 'TDF-B2B', branch: 'SFDEV' },
+  { owner: 'Saltbox-Mgmt', repo: 'DAIKIN-B2B', branch: 'dev' },
+  { owner: 'Saltbox-Mgmt', repo: 'Shorr-LWR-B2B', branch: 'dev' },
+  { owner: 'Saltbox-Mgmt', repo: 'Palecek-B2B', branch: 'dev' },
+  { owner: 'Saltbox-Mgmt', repo: 'GBP', branch: 'master' },
+  { owner: 'Saltbox-Mgmt', repo: 'ZO-B2B', branch: 'dev' },
+  { owner: 'Saltbox-Mgmt', repo: 'GordonCollege', branch: 'master' },
+  { owner: 'Saltbox-Mgmt', repo: 'Standard-Textile', branch: 'dev' },
+  { owner: 'Saltbox-Mgmt', repo: 'TTS-B2B', branch: 'main' }
 ];
 
 export function isRepoAllowed(owner: string, repo: string): boolean {
